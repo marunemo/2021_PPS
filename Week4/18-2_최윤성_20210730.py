6292d387043d26b18640c4d9f48d4c4c714de02d
marunemo <chys3697@gmail.com>
2021-07-30 02:24:41 +0900

    Reupload 18-2 python

--- Week4/18-2_최윤성_20210730.py
+++ Week4/18-2_최윤성_20210730.py
@@ -1,10 +1,15 @@
 money = int(input())
-count = {1:1, 2:1, 3:2, 4:2, 5:1, 6:2, 7:1}
+count = {0:0, 1:1, 2:1, 3:2, 4:2, 5:1, 6:2, 7:1}
 
 for i in range(8, money+1):
     minCount = i
-    for j in range(1, i):
-        if minCount > count[j] + count[i - j]:
-            minCount = count[j] + count[i - j]
+    if minCount > count[1] + count[i-1]:
+        minCount = count[1] + count[i-1]
+    if minCount > count[2] + count[i-2]:
+        minCount = count[2] + count[i-2]
+    if minCount > count[5] + count[i-5]:
+        minCount = count[5] + count[i-5]
+    if minCount > count[7] + count[i-7]:
+        minCount = count[7] + count[i-7]
     count[i] = minCount
 print(count[money])
