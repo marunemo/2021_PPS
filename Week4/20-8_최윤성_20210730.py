6242fbaf94cd7ccabc5a7c32f4e621a7f5b242ce
marunemo <chys3697@gmail.com>
2021-07-31 02:33:27 +0900

    solved 20-8

---
+++ Week4/20-8_최윤성_20210730.py
@@ -0,0 +1,11 @@
+n = int(input())
+people = []
+for _ in range(n):
+    people.append(list(map(int, input().split())))
+for i in range(n):
+    rank = 1
+    for j in range(n):
+        if people[i][0] < people[j][0] and people[i][1] < people[j][1]:
+            rank += 1
+    print(rank, end=" ")
+print()
